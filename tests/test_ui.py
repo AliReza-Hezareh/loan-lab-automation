a4b8053e74a5b977902a340d4e2310264be3621c
AliWanton <hezareh22@gmail.com>
2026-02-24 15:30:52 +0100

    Use short personnummer format in test
    Update tests/test_ui.py to use a YYMMDD-style personnummer value: changed "19900101-1234" to "900101-1234". This aligns the test input with the expected short-format personal ID used by the UI validation.

--- tests/test_ui.py
+++ tests/test_ui.py
@@ -25,7 +25,7 @@ def test_select_product(page):
     product_page.click_next()
     
     application_page.fylla_i_personuppgifter(
-        personnummer="19900101-1234",
+        personnummer="900101-1234",
         förnamn=fake.first_name(),
         efternamn=fake.last_name(),
         email=fake.email(),
