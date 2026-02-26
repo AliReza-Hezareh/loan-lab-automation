d0f190ce055ff9d1169f06724962d56ef21da630
Alireza-Hezareh <hezareh22@gmail.com>
2026-02-26 14:21:09 +0100

    Update product_page.py
    refactor(product_page): remove hardcoded URL and add method for filling personal information form

--- src/pages/product_page.py
+++ src/pages/product_page.py
@@ -1,13 +1,13 @@
 from playwright.sync_api import Page
+from src.config import GUI_BASE_URL
 
 
 class ProductPage:
     def __init__(self, page: Page):
         self.page = page
-        self.url = "https://souderbroder-loan-lab.lovable.app"
         
     def navigate(self):
-        self.page.goto(self.url)
+        self.page.goto(GUI_BASE_URL)
         
     def select_produkt(self, product_name: str):
         self.page.get_by_role("heading", name=product_name).click()
@@ -15,4 +15,8 @@ class ProductPage:
     def click_next(self):
         self.page.get_by_role("button", name="Nästa").click()
         
-        
+    def fyll_i_personuppgifter(self, first_name, last_name, personal_number, email):
+        self.page.fill("#personalNumber", personal_number)
+        self.page.fill("#firstName", first_name)
+        self.page.fill("#lastName", last_name)
+        self.page.fill("#email", email)
